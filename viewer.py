c4293e119f4eb83ad8198e68a2c26573f1cf135a
George Hotz <geohot@gmail.com>
2020-02-28 09:32:46 -0800

    viewer on image numbers

--- viewer.py
+++ viewer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 from common.window import Window
 import numpy as np
 from tqdm import tqdm
@@ -19,9 +20,13 @@ def gray_to_color(image):
   output = output.reshape((W, H, 3)).astype(np.uint8)
   return output
 
+
 if __name__ == "__main__":
   win = Window(1164, 874)
-  for x in tqdm(sorted(os.listdir("imgs/"))):
+  lst = sorted(os.listdir("imgs/"))
+  if len(sys.argv) > 1:
+    lst = list(filter(lambda x: x.startswith(("%04d" % int(sys.argv[1]))), lst))
+  for x in tqdm(lst):
     ii = np.array(Image.open("imgs/"+x))
     if not NOSEGS and os.path.isfile("segz/"+x+".npz"):
       out = np.load("segz/"+x+".npz")['arr_0']
