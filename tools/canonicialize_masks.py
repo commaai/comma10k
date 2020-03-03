12283daaaca063f92249590d42808ea669e8a241
George Hotz <geohot@gmail.com>
2020-03-03 10:18:57 -0800

    add color check to canonicialize_masks

--- tools/canonicialize_masks.py
+++ tools/canonicialize_masks.py
@@ -2,20 +2,35 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-from viewer import fix
+import numpy as np
+from viewer import fix, get_colormap
 from tqdm import tqdm
 from multiprocessing import Pool
 from PIL import Image
 
+colormap = get_colormap()
+
 def canon_mask(x):
   segi = fix(Image.open("masks/"+x))
   #print(x, segi.shape, segi.dtype)
+  check = segi.reshape(-1, 3)
+
+  ok = np.zeros(check.shape[0], dtype=np.bool)
+  for v in colormap.values():
+    okk = check == np.array(v)
+    okk = np.all(okk, axis=1)
+    ok |= okk
+
+  if not np.all(ok):
+    print(x+" HAS BAD COLORS")
+    print(check[np.logical_not(ok)])
+
   im = Image.fromarray(segi)
   im.save("masks/"+x)
 
 if __name__ == "__main__":
-  p = Pool(16)
   lst = sorted(os.listdir("masks/"))
+  p = Pool(16)
   for _ in tqdm(p.imap_unordered(canon_mask, lst), total=len(lst)):
     pass
 
