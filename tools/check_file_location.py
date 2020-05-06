cd4e64e61369269f6bd5295f5cbc70e6df9cc506
Joey Lao <pjlao307@gmail.com>
2020-05-06 08:31:01 -0700

    Add check for masks to not be in root folder

---
+++ tools/check_file_location.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+
+sys.path.append(os.getcwd())
+
+# This script checks to see if files are in the wrong folder (should be in /masks folder)
+
+# Only these files should exist in the repo
+static_list = ['.git', '.github', '.gitignore', 'LICENSE', 'README.md', 'files_trainable', 'imgs', 'masks', 'pencil', 'requirements.txt', 'sample.gif', 'sample.jpg', 'scale', 'stat.py', 'tools', 'viewer.py']
+
+def check_file(x):
+  bad = 1
+  if x in static_list:
+    bad = 0
+
+  return bad
+
+if __name__ == "__main__":
+  lst = sorted(os.listdir("./"))
+  
+  bads = []
+  
+  for bad in tqdm(map(check_file, lst), total=len(lst)):
+    bads.append(bad)
+  
+  if any(bads):
+    print("THERE ARE %d FILE(S) IN THE WRONG FOLDER" % sum(bads))
+    exit(-1)
+  else:
+    exit(0)
+	
