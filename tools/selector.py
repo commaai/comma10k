32d6b7c14b0c0e36c4c9917bf2cf367ebddc98c7
George Hotz <geohot@gmail.com>
2020-05-28 16:02:19 -0700

    add 300 hard images

--- tools/selector.py
+++ tools/selector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+import glob
 from window import Window
 import numpy as np
 sys.path.append(os.getcwd())
@@ -12,15 +13,28 @@ from PIL import Image
 # grep -vIr "\x00" -- */*/ent.txt > /tmp/ents
 # cat /tmp/ents | sort -t ":" -k2 -n -r > /tmp/entssort
 
-BASEDIR = "/raid.dell03/runner/comma10k/Comma10k"
+BASEDIR = sys.argv[1]
 
 win = Window(1164, 874)
-cc = 100
+cc = 0
 
-dat = open(sys.argv[1]).read().strip().split("\n")
+while len(glob.glob("imgs/h%03d*" % cc)) > 0:
+  cc += 1
+print("starting with %d" % cc)
+
+seen = set([x[len('imgs/h113_'):] for x in glob.glob("imgs/h*")])
+
+# permanent camera occulusions
+EXCLUDE_USERS = ["807f77aac0daa4b6", "84e6a31bffe59bee"]
+
+dat = open(sys.argv[2]).read().strip().split("\n")
 for d in tqdm(dat):
   fn = os.path.join(BASEDIR, d.split(":")[0].replace("/ent.txt", ""))
   dd = sorted(os.listdir(fn))
+  if dd[1][5:] in seen:
+    continue
+  if dd[1].split("_")[1] in EXCLUDE_USERS:
+    continue
   print(dd)
 
   ii = np.array(Image.open(os.path.join(fn, dd[1])))
