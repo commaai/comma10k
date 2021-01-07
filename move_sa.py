5498df9be0cedb016f8140a9a209087b8c0117be
Abdul Adhil PK <65992809+adhilcodes@users.noreply.github.com>
2021-01-07 06:39:56 +0530

    Update move_sa.py

--- move_sa.py
+++ move_sa.py
@@ -4,6 +4,6 @@ saimgs = sorted([x for x in os.listdir('imgs') if x.startswith("sa")])
 print(len(saimgs))
 
 for i,x in enumerate(saimgs):
-  os.rename("imgs/"+x, "imgs/%4d_%s" % (5000+i,x[8:]))
-  os.rename("masks/"+x, "masks/%4d_%s" % (5000+i,x[8:]))
+  os.rename("imgs/"+x, "imgs/%4d_%s" % (5000+i, x[8:]))
+  os.rename("masks/"+x, "masks/%4d_%s" % (5000+i, x[8:]))
 
