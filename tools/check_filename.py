8049f8932067c12e278d4c9dc70e5da7a1099691
pjlao307 <pjlao307@gmail.com>
2021-04-06 10:02:27 -0700

    Update check_filenames with new folders

--- tools/check_filename.py
+++ tools/check_filename.py
@@ -20,13 +20,16 @@ def check_file(x):
 
 if __name__ == "__main__":
   filenames = subprocess.check_output("git ls-files imgs | awk '{sub(/imgs\//,\"\"); print }'", shell=True).strip().split(b"\n")
-#  exit(0)
+  filenames = filenames + subprocess.check_output("git ls-files imgs2 | awk '{sub(/imgs2\//,\"\"); print }'", shell=True).strip().split(b"\n")
+
   lst = sorted(os.listdir("masks/"))
+  lst = lst + sorted(os.listdir("masks2/"))
+
   bads = []
   
   for bad in tqdm(map(check_file, lst), total=len(lst)):
     bads.append(bad)
-	
+
   if any(bads):
     print("THERE ARE %d FILE(S) WITH WRONG NAMES" % sum(bads))
     exit(-1)
